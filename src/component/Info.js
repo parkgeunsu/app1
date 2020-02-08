f38147c04b0a83b94adfbf45394830f2b9c0c9f6
parkgeunsu <parkgeunsu@naver.com>
2020-02-08 15:36:01 +0900

    .

--- src/component/Info.js
+++ src/component/Info.js
@@ -1,16 +1,14 @@
 import React,{Component,useState} from 'react';
 
-class Info extends Component{
-  render(){
-    const [value,setValue] = useState(0);
-    return (
-      <div>
-        <input type="button" value="+" onClick={()=>setValue(value+1)} />
-        <input type="button" value="-" onClick={()=> setValue(value-1)} />
-        카운터는 {value}입니다.
-      </div>
-    )
-  }
+const Info = () => {
+  const [value,setValue] = useState(0);
+  return (
+    <div>
+      <input type="button" value="+" onClick={()=>setValue(value+1)} />
+      <input type="button" value="-" onClick={()=>setValue(value-1)} />
+      카운터는 {value}입니다.
+    </div>
+  )
 }
 
 export default Info;
