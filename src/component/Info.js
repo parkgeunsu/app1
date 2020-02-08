0122e8ff86cf9dd948a853b079c35b1c691409a8
parkgeunsu <parkgeunsu@naver.com>
2020-02-08 17:08:55 +0900

    1

--- src/component/Info.js
+++ src/component/Info.js
@@ -1,14 +1,15 @@
-import React,{useState} from 'react';
+import React,{Component} from 'react';
+import Counter from './Counter';
 
-const Info = () => {
-  const [value,setValue] = useState(30);
-  return (
-    <div>
-      <input type="button" value="+" onClick={()=>setValue(value+1)} />
-      <input type="button" value="-" onClick={()=>setValue(value-1)} />
-      김세정 나이 {value}입니다.
-    </div>
-  )
+class Info extends Component{
+  render(){
+    return (
+      <div>
+        <Counter name="박근수" age="41"/>
+        <Counter name="김세정" age="30"/>
+      </div>
+    )
+  }
 }
 
 export default Info;
